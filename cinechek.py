5a6ee00f99ec4cf89098aae01bb124f8945f7f81
Thay <thaynararm2@hotmail.com>
2023-07-12 23:03:16 -0300

    Inserindo o Flask WTF e habilitando a proteção CSRF

--- cinechek.py
+++ cinechek.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
+
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
+csrf = CSRFProtect(app)
 db = SQLAlchemy(app)
 
 from views import *
